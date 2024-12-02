b5299db5fb9b98b266b0a50a54b59639bbac908d
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2024-12-02 07:56:02 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace EBSRamTemizleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(20, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(21, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Press \'ESC\' to Exit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(197, 85);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EBS Ram Temizleme Programı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using BekraRamclear;
+using System;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Timers;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace EBSRamTemizleme
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer timer;
+        private bool dragging = false;
+        private int dragStartX = 0;
+        private int dragStartY = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Arka plan rengini ayarla
+            this.BackColor = System.Drawing.Color.FromArgb(255, 28, 35, 42); // A: 255, R: 28, G: 35, B: 42
+
+            // Form Ayarları
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.ShowInTaskbar = false;
+    
+            //this.TopMost = true;
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new System.Drawing.Point(Screen.PrimaryScreen.WorkingArea.Width - this.Width, 0);
+
+            // Klavye olaylarını yakalamak için
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) // ESC tuşuna basıldığında
+            {
+                this.Close(); // Formu kapat
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Zamanlayıcı ayarları
+            this.timer = new System.Timers.Timer();
+            this.timer.Interval = 5 * 1000; // 5 saniye
+            this.timer.Elapsed += new ElapsedEventHandler(this.TimerElapsed);
+            this.timer.Start();
+
+            // Drag & Drop olayları
+            this.MouseDown += new MouseEventHandler(Form1_MouseDown);
+            this.MouseMove += new MouseEventHandler(Form1_MouseMove);
+            this.MouseUp += new MouseEventHandler(Form1_MouseUp);
+            this.Paint += ModernForm_Paint; // Formu özel olarak çizeceğiz.
+            label1.Text = GetRamBilgisiGB();
+        }
+        private void ModernForm_Paint(object sender, PaintEventArgs e)
+        {
+            // Formun yuvarlak köşeli tasarımı
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            // Yuvarlak köşe oluştur
+            GraphicsPath path = new GraphicsPath();
+            int borderRadius = 30;
+            path.AddArc(0, 0, borderRadius, borderRadius, 180, 90);
+            path.AddArc(this.Width - borderRadius, 0, borderRadius, borderRadius, 270, 90);
+            path.AddArc(this.Width - borderRadius, this.Height - borderRadius, borderRadius, borderRadius, 0, 90);
+            path.AddArc(0, this.Height - borderRadius, borderRadius, borderRadius, 90, 90);
+            path.CloseAllFigures();
+
+            // Gradient arkaplan
+            LinearGradientBrush gradientBrush = new LinearGradientBrush(
+                this.ClientRectangle,
+                Color.FromArgb(45, 45, 48), // Gradient başlangıç rengi
+                Color.FromArgb(28, 28, 30), // Gradient bitiş rengi
+                LinearGradientMode.ForwardDiagonal);
+
+            g.FillPath(gradientBrush, path);
+
+            // Kenar efekti (parlayan hat)
+            Pen borderPen = new Pen(Color.FromArgb(128, 255, 255, 255), 2);
+            g.DrawPath(borderPen, path);
+
+            // Form bölgesini ayarla
+            this.Region = new Region(path);
+
+            // Gölgeler
+            AddShadow(g);
+        }
+        public static string GetRamBilgisiGB()
+        {
+            var toplamBellekSayacı = new PerformanceCounter("Memory", "Available MBytes");
+            var kullanılabilirRamGB = toplamBellekSayacı.NextValue() / 1024.0; // GB'ye çevir
+            var toplamRamGB = GetirToplamFizikselBellekBayt() / (1024.0 * 1024.0 * 1024.0); // GB'ye çevir
+            var kullanılanRamGB = toplamRamGB - kullanılabilirRamGB;
+
+            return $"Kullanılan RAM: {kullanılanRamGB:F2} GB\n";
+        }
+
+        private static long GetirToplamFizikselBellekBayt()
+        {
+            using (var arama = new System.Management.ManagementObjectSearcher("SELECT TotalVisibleMemorySize FROM Win32_OperatingSystem"))
+            {
+                foreach (var obje in arama.Get())
+                {
+                    return Convert.ToInt64(obje["TotalVisibleMemorySize"]) * 1024; // Bayt cinsinden
+                }
+            }
+
+            return 0; // Varsayılan değer
+        }
+        private void AddShadow(Graphics g)
+        {
+            int shadowSize = 15;
+            Rectangle shadowRect = new Rectangle(
+                shadowSize / 2,
+                shadowSize / 2,
+                this.Width - shadowSize,
+                this.Height - shadowSize);
+
+            using (GraphicsPath shadowPath = new GraphicsPath())
+            {
+                shadowPath.AddRectangle(shadowRect);
+
+                using (PathGradientBrush shadowBrush = new PathGradientBrush(shadowPath))
+                {
+                    shadowBrush.CenterColor = Color.FromArgb(120, 0, 0, 0); // Ortada daha yoğun gölge
+                    shadowBrush.SurroundColors = new Color[] { Color.Transparent }; // Kenarlarda şeffaflık
+
+                    g.FillRectangle(shadowBrush, shadowRect);
+                }
+            }
+        }
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                label1.Text = GetRamBilgisiGB();
+                BekraRamClearr.temizle();
+            });
+        }
+
+        // Form sürükleme için olaylar
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                dragging = true;
+                dragStartX = e.X;
+                dragStartY = e.Y;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                this.Left += e.X - dragStartX;
+                this.Top += e.Y - dragStartY;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                dragging = false;
+            }
+        }
+    }
+}
